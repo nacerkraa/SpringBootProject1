737b87f1f689943c144edfafd55de558f7d91ce1
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-12-05 02:18:56 +0100

    Add main files

---
+++ src/main/java/com/example/demo/SpringBootProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject1Application.class, args);
+	}
+
+}
